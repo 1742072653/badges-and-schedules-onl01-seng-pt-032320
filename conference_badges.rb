33b20269ac0e7b900f4a57125144ff7efbf2bd6b
Wenhao Guo <gwh1742072653@gmail.com>
2020-03-27 19:49:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badge = []
+  names.each do |name|
+    badge. << "Hello, #{name}."
+  end
+    return badge
+    
+    def assing_rooms(names)
+      count = 1 
+      rooms = []
+      name. each do |name|
+        rooms <<
